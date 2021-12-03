ad511edcc95ac0852cb8a722bd70e2236ca5a3fa
Jacob Asuncion <asuncion1173@gmail.com>
2021-12-03 16:43:49 +0800

    Updated format and added Covid-19 Directories

--- ContactViewer/Form1.Designer.cs
+++ ContactViewer/Form1.Designer.cs
@@ -29,70 +29,124 @@ namespace ContactViewer
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.labelContact = new System.Windows.Forms.Label();
             this.richTextBox = new System.Windows.Forms.RichTextBox();
             this.textBoxSearch = new System.Windows.Forms.TextBox();
             this.btnSearch = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // labelContact
             // 
             this.labelContact.AutoSize = true;
-            this.labelContact.Location = new System.Drawing.Point(238, 26);
+            this.labelContact.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelContact.Location = new System.Drawing.Point(93, 9);
             this.labelContact.Name = "labelContact";
-            this.labelContact.Size = new System.Drawing.Size(118, 13);
+            this.labelContact.Size = new System.Drawing.Size(206, 24);
             this.labelContact.TabIndex = 0;
             this.labelContact.Text = "Contact Tracing Viewer";
             // 
             // richTextBox
             // 
-            this.richTextBox.Location = new System.Drawing.Point(12, 42);
+            this.richTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox.Location = new System.Drawing.Point(34, 42);
             this.richTextBox.Name = "richTextBox";
-            this.richTextBox.Size = new System.Drawing.Size(574, 231);
-            this.richTextBox.TabIndex = 1;
+            this.richTextBox.Size = new System.Drawing.Size(310, 300);
+            this.richTextBox.TabIndex = 100;
             this.richTextBox.Text = "";
             // 
             // textBoxSearch
             // 
-            this.textBoxSearch.Location = new System.Drawing.Point(110, 303);
+            this.textBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSearch.Location = new System.Drawing.Point(34, 359);
             this.textBoxSearch.Name = "textBoxSearch";
-            this.textBoxSearch.Size = new System.Drawing.Size(202, 20);
-            this.textBoxSearch.TabIndex = 2;
+            this.textBoxSearch.Size = new System.Drawing.Size(202, 21);
+            this.textBoxSearch.TabIndex = 1;
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(339, 303);
+            this.btnSearch.Location = new System.Drawing.Point(269, 357);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(75, 23);
-            this.btnSearch.TabIndex = 3;
+            this.btnSearch.TabIndex = 2;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
             this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
             // 
             // btnClear
             // 
-            this.btnClear.Location = new System.Drawing.Point(420, 303);
+            this.btnClear.Location = new System.Drawing.Point(269, 398);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(75, 23);
-            this.btnClear.TabIndex = 4;
+            this.btnClear.TabIndex = 3;
             this.btnClear.Text = "Clear";
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(419, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 101;
+            this.label1.Text = "COVID-19 Directory ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(353, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(233, 39);
+            this.label2.TabIndex = 102;
+            this.label2.Text = "Department of Health (DOH)\r\n02-894-COVID (02-894-26843)\r\n1555 (PLDT, Smart, Sun, " +
+    "and TNT Subscribers)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(353, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 26);
+            this.label3.TabIndex = 103;
+            this.label3.Text = "Philippine Red Cross\r\nHotline 1158";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(353, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(216, 364);
+            this.label4.TabIndex = 104;
+            this.label4.Text = resources.GetString("label4.Text");
             // 
             // Form1
             // 
             this.AcceptButton = this.btnSearch;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(598, 370);
+            this.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.ClientSize = new System.Drawing.Size(598, 514);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.textBoxSearch);
             this.Controls.Add(this.richTextBox);
             this.Controls.Add(this.labelContact);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Safe Stay Viewer";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -105,6 +159,10 @@ namespace ContactViewer
         private System.Windows.Forms.TextBox textBoxSearch;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
